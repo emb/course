460edef895ab11453ab3c1af010102e2effa3ffb
Ed M Barwani <ed.barwani@gmail.com>
2015-10-11 10:06:46 +1100

    Implement List.produce

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -318,8 +318,7 @@ produce ::
   (a -> a)
   -> a
   -> List a
-produce =
-  error "todo: Course.List#produce"
+produce f a = a :. map f (produce f a)
 
 -- | Do anything other than reverse a list.
 -- Is it even possible?
