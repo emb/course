cd0261cf0c12e71f3df077027af701d52a4e0c05
Ed M Barwani <ed.barwani@gmail.com>
2015-11-21 08:35:20 +1100

    Implement Optional Bind

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -109,7 +109,7 @@ instance Monad Optional where
     -> Optional a
     -> Optional b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+    bindOptional
 
 -- | Binds a function on the reader ((->) t).
 --
